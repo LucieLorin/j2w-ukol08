ae4f7055a9fec8a6de12d5a4b08dc213ae5d1d23
Lucie Bazantova <luciebaz@email.cz>
2021-06-02 16:14:23 +0200

    Upgrade

---
+++ src/main/resources/db/migration/POST.sql
@@ -0,0 +1,6 @@
+INSERT INTO PUBLIC.POST (ID, SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED) VALUES (54, 'Prague', 'anonymous', 'Prime minister arrested', 'News', 'Mé zatčení je kampaaaaaň', '2021-10-27');
+INSERT INTO PUBLIC.POST (ID, SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED) VALUES (545, 'Cthulhu', 'Lovecraft', 'Volání Cthulhu', 'Moshi - moshi????', 'Tentacle', '2021-06-01');
+INSERT INTO PUBLIC.POST (ID, SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED) VALUES (857, 'Cirilla', 'Vlaštovka', 'Ciri', 'Cintra', 'Lvíče z Cintry.', '2021-06-01');
+INSERT INTO PUBLIC.POST (ID, SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED) VALUES (888, 'Chernobyl', 'SSSR', 'Bombastický job', '3,6', 'Not great, not terrible.', '1986-04-26');
+INSERT INTO PUBLIC.POST (ID, SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED) VALUES (8578, 'Divadlo', 'Jára Cimrman', 'Lijavec', 'Hospoda', 'Koupil si hospodu, ale chodili mu tam lidi!!!', '2021-06-02');
+INSERT INTO PUBLIC.POST (ID, SLUG, AUTHOR, TITLE, PEREX, BODY, PUBLISHED) VALUES (8854, 'Klíče', 'Chroustal', 'Chroustal se ztratil.', 'Štola', 'Vy jste bez prémií, ale my jsme bez Chroustala!', '2021-07-05');
